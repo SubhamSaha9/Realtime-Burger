de34f4652a3cc199efcb665788116e0be20d58e7
SubhamSaha9 <sahasubham7478@gmail.com>
2024-03-24 20:09:14 +0530

    initial

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const port = process.env.PORT || 8080;
+
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+
+app.listen(port, () => {
+    console.log("app is listening to port no 8080");
+})
