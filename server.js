2ec175fb6631089e59c8d950de96efb57a9486bd
SubhamSaha9 <sahasubham7478@gmail.com>
2024-03-25 22:42:32 +0530

    add cart, auth page

--- server.js
+++ server.js
@@ -6,15 +6,22 @@ const expressLayout = require("express-ejs-layouts");
 const port = process.env.PORT || 8080;
 
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-    res.render("home.ejs");
-})
-
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+})
+app.get("/cart", (req, res) => {
+    res.render("customers/cart.ejs");
+})
+app.get("/login", (req, res) => {
+    res.render("auth/login.ejs");
+})
+app.get("/register", (req, res) => {
+    res.render("auth/register.ejs");
+})
 
 app.listen(port, () => {
     console.log("app is listening to port no 8080");
