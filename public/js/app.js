99e6186c18757dbb222cc2129d8382b34c5755c1
SubhamSaha9 <sahasubham7478@gmail.com>
2024-04-09 23:44:59 +0530

    make responsive

--- public/js/app.js
+++ public/js/app.js
@@ -149,12 +149,12 @@ function updateStatus(order) {
 }
 updateStatus(order);
 var socket = io();
-(0,_admin__WEBPACK_IMPORTED_MODULE_1__.initAdmin)(socket);
 if (order) {
   socket.emit("join", "order_".concat(order._id));
 }
 var adminAreaPath = window.location.pathname;
 if (adminAreaPath.includes("admin")) {
+  (0,_admin__WEBPACK_IMPORTED_MODULE_1__.initAdmin)(socket);
   socket.emit("join", "adminRoom");
 }
 socket.on("orderUpdated", function (data) {
